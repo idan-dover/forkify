201896b2ef9bdeb792305adfb2be95d516e6ddae
Idan Dover <idan.dava@gmail.com>
2023-06-05 10:59:26 +0300

    redo the check

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,9 +8,9 @@ import "regenerator-runtime/runtime";
 
 // https://forkify-api.herokuapp.com/v2
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
